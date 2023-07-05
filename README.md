ccef629ab6b035d8fb3376661e215bc907cbe103
Gibran Hemani <explodecomputer@gmail.com>
2023-07-05 22:40:26 +0100

    upd

--- README.md
+++ README.md
@@ -24,5 +24,19 @@ Rscript run_cis_coloc.r
 Rscript collate_results.r
 ```
 
+Analysis of forward MR
+
+```
+quarto render docs/all_cis_mr.qmd
+```
+
+Analysis of reverse MR
+
+```
+Rscript extract_reverse_mr.r
+```
+
+
+
 Document here: https://uob-my.sharepoint.com/:w:/r/personal/ph14916_bristol_ac_uk/_layouts/15/guestaccess.aspx?e=4%3ANpXgxH&at=9&share=EebxUnfMH-lLm2HGxiKId9EB5jzG0jr8uDUstUMACBPmIQ
 
---
+++ extract_reverse_mr.r
@@ -0,0 +1,24 @@
+library(ieugwasr)
+library(dplyr)
+library(readxl)
+library(here)
+
+load(here("data", "all.rdata"))
+
+head(prs_pairs)
+
+# Get instruments for all opengwasid in prs_pairs
+
+inst <- ieugwasr::tophits(unique(prs_pairs$opengwasid))
+inst
+
+lookups <- left_join(
+    prs_pairs %>% select(prot, id=opengwasid),
+    inst %>% select(chr, position, rsid, ea, nea, id),
+    by="id",
+    relationship="many-to-many"
+)
+
+lookups
+saveRDS(lookups, file=here("data", "reverse_mr_lookups.rds"))
+
